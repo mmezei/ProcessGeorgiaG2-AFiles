791f1d3e42265ee9c102e76c3ba1e4fff13f52bd
momez <mmezei5@gmail.com>
2022-05-23 20:54:11 -0400

    Update GenerateGeorgiaG2AFiles.js

--- GenerateGeorgiaG2AFiles.js
+++ GenerateGeorgiaG2AFiles.js
@@ -1,32 +1,4 @@
-// INSTRUCTIONS:
-// * First note all comments below
-//
-// 1. Copy the G2A folder to c:\Users\YourUserName\Desktop\G2A
-//
-// 2. Add rows to G2AData.csv
-//    Strict formatting rules are required apparently. We don't attempt to do it programmatically due to possible differences in input data (SS# vs TIN, etc.)
-//    * PAYERS NONRESIDENTIAL NR WH: No Dashes 1234567AB
-//    * PAYERS FEDERAL ID NUMBER: Must have Dashes 12-1234567
-//    * RECIPIENTS FEINID NUMBER: No Dashes 121234567
-//    * NO COMMAS IN DATA FIELDS/NUMBERS - it is used as the column delimiter.
-//
-// 3. Copy G2AData.csv and GenerateGeorgiaG2AFiles.js to  C:\Program Files (x86)\Adobe\Acrobat DC\Acrobat\Javascripts
-//
-// 4. Open Acrobat
-//
-// 5. Enable Javascript as follows in Edit/Preferences/JavaScript:
-//    * Enable Acrobat Javascript
-//    * Enable menu items Javascript execution policies
-//    * Enable global object security policy
-//    * Click Ok
-//    * Restart Acrobat
-//
-// 6. Open the G2A Pdf Form (only tested on form Rev. 05/27/16) from the G2A folder on your desktop
-//
-// 7. In Edit Menu click "Generate Georgia G2A Files"
-//
-// 8. Click dialogs as appropriate and pdf's will be added to the G2A folder on your desktop
-//
+
 var GenerateGeorgiaG2AFiles = app.trustedFunction(function () {
   var fileName =
     "/C/Program Files (x86)/Adobe/Acrobat DC/Acrobat/Javascripts/G2AData.csv";
